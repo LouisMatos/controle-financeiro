78b82f6b67774335894e7a29dfd9156b5938e2ff
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2022-04-11 12:08:59 -0300

    first commit

---
+++ src/main/java/br/com/luismatos/controlefinanceiro/controller/IndexController.java
@@ -0,0 +1,18 @@
+package br.com.luismatos.controlefinanceiro.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@GetMapping
+	public String home(Model model, Principal principal) {
+		return "index";
+	}
+}

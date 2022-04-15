1205efe8bfbd0f2d0aedaae12dea75e27392b629
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2022-04-15 16:44:43 -0300

    ajustes no front end, ordenação de datas

--- src/main/java/br/com/luismatos/controlefinanceiro/repository/ArquivoRepository.java
+++ src/main/java/br/com/luismatos/controlefinanceiro/repository/ArquivoRepository.java
@@ -15,7 +15,7 @@ public interface ArquivoRepository extends JpaRepository<Transacao, Long> {
 	@Query(value = "SELECT count(*) FROM transacoes WHERE data_transacao LIKE %:dataTransacao%", nativeQuery = true)
 	Integer findByDataTransacao(@Param("dataTransacao") LocalDate dataTransacao);
 	
-		@Query(value = "SELECT distinct  t.data_transacao as dataTransacoes, DATE_FORMAT(t.data_importacao_transacoes, '%d/%m/%Y - %H:%i:%s') as dataImportacao FROM transacoes t;", nativeQuery = true)
+		@Query(value = "SELECT distinct  t.data_transacao as dataTransacoes, t.data_importacao_transacoes as dataImportacao FROM transacoes t;", nativeQuery = true)
 		List<ImportacaoRealizadaDTO> findDataTRansacaoDataImportacao();
 	
 	
